25b322a70b2eabf7acafd1a4fac1584b53ab8505
Mike <themcgamer9@gmail.com>
2025-10-04 14:39:48 -0400

    Initial Commit

---
+++ EventFinder/EventFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventFinderApp.swift
+//  EventFinder
+//
+//  Created by Reginald Coleman on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EventFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
